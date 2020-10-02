8e58185bb4e68fcb9367cd39ba150c2938c3afc5
Alessio Borraccino <alessio.borraccino@data4life.care>
2020-10-02 16:05:06 +0200

    Changes type of library to dynamic

--- Package.swift
+++ Package.swift
@@ -3,20 +3,21 @@
 import PackageDescription
 
 let package = Package(
-  name: "CryptoSwift",
-  platforms: [
-    .macOS(.v10_12), .iOS(.v9), .tvOS(.v9)
-  ],
-  products: [
-    .library(
-      name: "CryptoSwift",
-      targets: ["CryptoSwift"]
-    )
-  ],
-  targets: [
-    .target(name: "CryptoSwift"),
-    .testTarget(name: "CryptoSwiftTests", dependencies: ["CryptoSwift"]),
-    .testTarget(name: "TestsPerformance", dependencies: ["CryptoSwift"])
-  ],
-  swiftLanguageVersions: [.v5]
+    name: "CryptoSwift",
+    platforms: [
+        .macOS(.v10_12), .iOS(.v9), .tvOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "CryptoSwift",
+            type: .dynamic,
+            targets: ["CryptoSwift"]
+        )
+    ],
+    targets: [
+        .target(name: "CryptoSwift"),
+        .testTarget(name: "CryptoSwiftTests", dependencies: ["CryptoSwift"]),
+        .testTarget(name: "TestsPerformance", dependencies: ["CryptoSwift"])
+    ],
+    swiftLanguageVersions: [.v5]
 )
